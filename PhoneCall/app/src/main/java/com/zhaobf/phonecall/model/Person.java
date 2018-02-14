20476fa0573a41dbce9b4d018b3dce122801a95e
zbf <zbf@timevale.com>
2018-02-14 10:18:41 +0800

    开发拔号器

---
+++ PhoneCall/app/src/main/java/com/zhaobf/phonecall/model/Person.java
@@ -0,0 +1,44 @@
+package com.zhaobf.phonecall.model;
+
+/**
+ * Created by zhaobf on 2018/2/13.
+ */
+
+public class Person {
+    public Integer id;
+    public String number;
+    public String name;
+    public String headImage;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHeadImage() {
+        return headImage;
+    }
+
+    public void setHeadImage(String headImage) {
+        this.headImage = headImage;
+    }
+}
